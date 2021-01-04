c587a609be3dc97e941f5c3d8d7475e6dc5456c2
Matviienko Artem <k2141092mav@gmail.com>
2021-01-04 20:43:55 +0200

    Добавил задачу с CodeWars

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import './styles.css';
+
+
+///  ------------------- Задача с Codewars -------------------------
+
+function digitalRoot(namber) {
+  const namberString = String(namber);
+  // const newArray = namberString.split('').map(namber => Number(namber));
+
+  let x = 0;
+  console.log(x.length);
+  // while (newArray.length >) {}
+
+  if (namberString.length === 2) {
+    x = parseInt(namberString[0]) + parseInt(namberString[1]);
+
+    return console.log(x);
+  } else if (namberString.length > 2) {
+    // ------------------- этот вариант надо еще доделать ---------------
+    // for (let i = 0; i < namberString.length; i++) {
+    //   // arr.push(newArray[i]);
+
+    //   x += parseInt(namberString[i]);
+
+    //   console.log(`Это длинна ${namberString.length}`);
+    //   console.log(`Текущая цифра ${parseInt(namberString[i])}`);
+    // }
+
+    // while (x.length < 2) {}  <<<------------------ этот вариант надо еще продумать
+    return console.log(x);
+  }
+}
+
+digitalRoot(65);
+
+let counter = 5;
+
+while (counter < 55) {
+  console.log('counter: ', counter);
+  counter += 1;
+}
